ec9121f08ec5545ca73d4414398fd7b7a63ad468
parexcellencesystems <dwoods@parexcellencesystems.com>
2015-08-21 10:12:14 -0400

    Another commit

--- www/js/notification.js
+++ www/js/notification.js
@@ -1,29 +1,30 @@
-cordova.define("com.parexcellencesystems.notification.NotificationPlugin", function(require, exports, module) { var notification = {
-    start: function(apiUrl, authorizationHeader, intervalMillis, title, icon, successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'NotificationPlugin',
-            'start', 
-            [{
-                "apiUrl": apiUrl,
-                "authorizationHeader": authorizationHeader,
-                "intervalMillis": intervalMillis,
-                "title": title,
-                "icon": icon
-            }]
-        );
-    },
+cordova.define("com.parexcellencesystems.notification.NotificationPlugin", function(require, exports, module) {
+	var notification = {
+	    start: function(apiUrl, authorizationHeader, intervalMillis, title, icon, successCallback, errorCallback) {
+	        cordova.exec(
+	            successCallback,
+	            errorCallback,
+	            'NotificationPlugin',
+	            'start', 
+	            [{
+	                "apiUrl": apiUrl,
+	                "authorizationHeader": authorizationHeader,
+	                "intervalMillis": intervalMillis,
+	                "title": title,
+	                "icon": icon
+	            }]
+	        );
+	    },
 
-    stop: function(successCallback, errorCallback) {
-        cordova.exec(
-            successCallback,
-            errorCallback,
-            'NotificationPlugin',
-            'stop'
-        );
-    }
-}
+	    stop: function(successCallback, errorCallback) {
+	        cordova.exec(
+	            successCallback,
+	            errorCallback,
+	            'NotificationPlugin',
+	            'stop'
+	        );
+	    }
+	}
 
-module.exports = notification;
+	module.exports = notification;
 });
